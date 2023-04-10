0cbc0c2fd35bc78e5176d945549a128038c30fa3
Luis Magana <luis.a.magana1@gmail.com>
2023-04-09 18:02:49 -0700

    Complete Q5

--- lab/hw04/hw04.py
+++ lab/hw04/hw04.py
@@ -1,5 +1,6 @@
 this_file = __file__
 
+
 # Mobiles
 
 def mobile(left, right):
@@ -8,61 +9,73 @@ def mobile(left, right):
     assert is_arm(right), "right must be a arm"
     return ['mobile', left, right]
 
+
 def is_mobile(m):
     """Return whether m is a mobile."""
     return type(m) == list and len(m) == 3 and m[0] == 'mobile'
+
 
 def left(m):
     """Select the left arm of a mobile."""
     assert is_mobile(m), "must call left on a mobile"
     return m[1]
 
+
 def right(m):
     """Select the right arm of a mobile."""
     assert is_mobile(m), "must call right on a mobile"
     return m[2]
+
 
 def arm(length, mobile_or_planet):
     """Construct a arm: a length of rod with a mobile or planet at the end."""
     assert is_mobile(mobile_or_planet) or is_planet(mobile_or_planet)
     return ['arm', length, mobile_or_planet]
 
+
 def is_arm(s):
     """Return whether s is a arm."""
     return type(s) == list and len(s) == 3 and s[0] == 'arm'
+
 
 def length(s):
     """Select the length of a arm."""
     assert is_arm(s), "must call length on a arm"
     return s[1]
 
+
 def end(s):
     """Select the mobile or planet hanging at the end of a arm."""
     assert is_arm(s), "must call end on a arm"
     return s[2]
+
 
 def planet(size):
     """Construct a planet of some size."""
     assert size > 0
     return ['planet', size]
 
+
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
     return w[1]
 
+
 def is_planet(w):
     """Whether w is a planet."""
     return type(w) == list and len(w) == 2 and w[0] == 'planet'
+
 
 def examples():
     t = mobile(arm(1, planet(2)),
                arm(2, planet(1)))
     u = mobile(arm(5, planet(1)),
                arm(1, mobile(arm(2, planet(3)),
-                              arm(3, planet(2)))))
+                             arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
     return (t, u, v)
+
 
 def total_weight(m):
     """Return the total weight of m, a planet or mobile.
@@ -80,6 +93,7 @@ def total_weight(m):
     else:
         assert is_mobile(m), "must get total weight of a mobile or a planet"
         return total_weight(end(left(m))) + total_weight(end(right(m)))
+
 
 def balanced(m):
     """Return whether m is balanced.
@@ -173,6 +187,8 @@ def replace_leaf(t, old, replacement):
     else:
         laerad = [replace_leaf(b, old, replacement) for b in branches(t)]
         return tree(label(t), laerad)
+
+
 def make_withdraw(balance, password):
     """Return a password-protected withdraw function.
 
@@ -201,7 +217,22 @@ def make_withdraw(balance, password):
     >>> type(w(10, 'l33t')) == str
     True
     """
-    "*** YOUR CODE HERE ***"
+    incorrect_pass = []
+    
+    def withdraw(amount, key):
+        nonlocal balance, password
+        if len(incorrect_pass) == 3:
+            return "Your account is locked. Attempts: " + str(incorrect_pass)
+        elif key is not password:
+            incorrect_pass.append(key)
+            return 'Incorrect password'
+        elif amount > balance:
+            return 'Insufficient funds'
+        balance = balance - amount
+        return balance
+
+    return withdraw
+
 
 def make_joint(withdraw, old_pass, new_pass):
     """Return a password-protected withdraw function that has joint access to
@@ -243,6 +274,7 @@ def make_joint(withdraw, old_pass, new_pass):
     """
     "*** YOUR CODE HERE ***"
 
+
 ## Tree Methods ##
 
 def tree(label, branches=[]):
@@ -251,13 +283,16 @@ def tree(label, branches=[]):
         assert is_tree(branch), 'branches must be trees'
     return [label] + list(branches)
 
+
 def label(tree):
     """Return the label value of a tree."""
     return tree[0]
 
+
 def branches(tree):
     """Return the list of branches of the given tree."""
     return tree[1:]
+
 
 def is_tree(tree):
     """Returns True if the given tree is a tree, and False otherwise."""
@@ -268,11 +303,13 @@ def is_tree(tree):
             return False
     return True
 
+
 def is_leaf(tree):
     """Returns True if the given tree's list of branches is empty, and False
     otherwise.
     """
     return not branches(tree)
+
 
 def print_tree(t, indent=0):
     """Print a representation of this tree in which each node is
@@ -297,6 +334,7 @@ def print_tree(t, indent=0):
     for b in branches(t):
         print_tree(b, indent + 1)
 
+
 def copy_tree(t):
     """Returns a copy of t. Only for testing purposes.
 
@@ -308,6 +346,7 @@ def copy_tree(t):
     """
     return tree(label(t), [copy_tree(b) for b in branches(t)])
 
+
 ###################
 # Extra Questions #
 ###################
@@ -316,18 +355,22 @@ def interval(a, b):
     """Construct an interval from a to b."""
     return [a, b]
 
+
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
 
+
 def str_interval(x):
     """Return a string representation of interval x.
     """
     return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+
 
 def add_interval(x, y):
     """Return an interval that contains the sum of any value in interval x and
@@ -335,6 +378,7 @@ def add_interval(x, y):
     lower = lower_bound(x) + lower_bound(y)
     upper = upper_bound(x) + upper_bound(y)
     return interval(lower, upper)
+
 
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
@@ -345,21 +389,25 @@ def mul_interval(x, y):
     p4 = x[1] * y[1]
     return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
 
+
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
-    reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
+    reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
+
 
 def par1(r1, r2):
     return div_interval(mul_interval(r1, r2), add_interval(r1, r2))
+
 
 def par2(r1, r2):
     one = interval(1, 1)
@@ -367,8 +415,10 @@ def par2(r1, r2):
     rep_r2 = div_interval(one, r2)
     return div_interval(one, add_interval(rep_r1, rep_r2))
 
+
 def multiple_references_explanation():
     return """The multiple reference problem..."""
+
 
 def quadratic(x, a, b, c):
     """Return the interval that is the range of the quadratic defined by
